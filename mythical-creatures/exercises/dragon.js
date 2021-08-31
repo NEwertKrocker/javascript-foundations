4178be900ab6f8d3249fcb51dc76e113f541fee1
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-08-31 13:48:30 -0400

    Add completed dragon.js and test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,19 @@
+class Dragon {
+  constructor(name, rider){
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.mealsEaten = 0;
+    }
+  greet() {
+    return `Hi, ${this.rider}!`;
+    };
+  eat() {
+    this.mealsEaten = this.mealsEaten + 1;
+    if (this.mealsEaten > 2){
+      this.hungry = false;
+    }
+  };
+};
 
+module.exports = Dragon;

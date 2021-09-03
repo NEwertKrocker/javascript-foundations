6a73b93e6026d99fadee41c253b9a25441998acb
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-09-02 22:48:14 -0400

    Add completed medusa.js and accompanying files

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,24 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name){
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victim){
+    var petrified = new Statue(victim.name);
+    if (this.statues.length < 3){
+      this.statues.push(petrified);
+    } else {
+      var person = new Person(this.statues[0].name);
+      person.mood = 'relieved';
+      this.statues.shift(petrified);
+      this.statues.push(petrified);
+      return person;
+    }
+  }
+}
+
+module.exports =
+Medusa;

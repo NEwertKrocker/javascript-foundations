205a008d24a7ed2905f0ed7d859cec6c3b34a669
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-09-20 17:36:00 -0400

    Add completed roadrace files

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,23 @@
 class Runner {
-
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
+  runSomeMiles(numMiles){
+    this.milesRun = this.milesRun + numMiles;
+    this.fitness = this.fitness + numMiles;
+  }
+  stretch(){
+    this.fitness = this.fitness + .5;
+  }
+  runRace(raceName, miles){
+    this.completedRaces.push(raceName);
+    this.milesRun += miles;
+    this.fitness += miles;
+  }
 }
 
 module.exports = Runner;
